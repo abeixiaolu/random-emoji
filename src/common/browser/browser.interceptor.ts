acd14832125777021c967647b7c5f21fa009905c
abei <abeixiaolu@gmail.com>
2025-04-17 14:08:47 +0800

    interceptor

---
+++ src/common/browser/browser.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class BrowserInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const userAgent = request.headers['user-agent'];
+    const browser = userAgent?.split(' ')[0] || 'Unknown';
+    request.browser = browser;
+    console.log('Browser:', browser);
+    return next.handle();
+  }
+}
